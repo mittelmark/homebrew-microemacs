3cd6976622dd45af839de4f5b35338403f32aaaf
Detlef Groth <75636+mittelmark@users.noreply.github.com>
2024-09-03 16:43:18 +0200

    adding macos url and sha256 sigest to formula for me09

--- Formula/me09.rb
+++ Formula/me09.rb
@@ -5,8 +5,8 @@ class Me09 < Formula
   version "24b2"
 
   if OS.mac?
-    url "file:///home/dgroth/workspace/microemacs/brew-macos-091224b2.tar.gz"
-    sha256 "macos-binary-sha256"
+    url "https://github.com/user-attachments/files/16850517/brew-macos-091224b2.tar.gz"
+    sha256 "ca6589dacdd58c44b829c7831463898c9900493e44ec1a4fe9f8f672ab14d9ef"
   elsif OS.linux?
     url "https://github.com/user-attachments/files/16850143/brew-linux-091224b2.tar.gz"
     sha256 "f3ce85246419cee2afd16ec48586c641f38a5370e439a9445f0b6805a00934e3"
