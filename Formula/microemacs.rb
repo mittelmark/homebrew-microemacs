c5e7d9c2d927ed77f88d2e2f5d2ac264f0c873f2
Detlef Groth <75636+mittelmark@users.noreply.github.com>
2024-09-10 14:41:56 +0200

    adding generic microemacs formula

--- Formula/microemacs.rb
+++ Formula/microemacs.rb
@@ -12,6 +12,7 @@ class Microemacs < Formula
     # Define the target directory
     target_dir = "#{ENV['HOME']}/.config/jasspa"
     mkdir_p target_dir unless File.exist?(target_dir)
+    print(target_dir)
     system "curl -L -o help_ehf.zip https://github.com/bjasspa/jasspa/releases/download/me_20240902/Jasspa_MicroEmacs_20240902_help_ehf.zip"
     # Unzip the downloaded file
     system "unzip -o help_ehf.zip -d #{buildpath}"
@@ -22,7 +23,7 @@ class Microemacs < Formula
     # Install help file
     cp "#{buildpath}/macros/me.ehf", target_dir
     # Clean up the downloaded ZIP file
-    rm_f "help_ehf.zip"    # Create the target directory if it doesn't exist
+    rm_f "help_ehf.zip"    
   end
 
 end
