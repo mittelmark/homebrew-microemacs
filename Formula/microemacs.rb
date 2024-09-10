88c87735c9bc7fac0d649beb788359737e4fa349
Detlef Groth <75636+mittelmark@users.noreply.github.com>
2024-09-10 14:28:13 +0200

    adding generic microemacs formula

--- Formula/microemacs.rb
+++ Formula/microemacs.rb
@@ -11,11 +11,21 @@ class Microemacs < Formula
   sha256 "CC95587293B33B0805906D1143CF473F419325F493E146C695C6325A45D56D27"
 
   def install
+      
   # Define the target directory
     target_dir = "#{ENV['HOME']}/.config/jasspa"
-    # Create the target directory if it doesn't exist
     mkdir_p target_dir unless File.exist?(target_dir)
-    cp "#{buildpath}/macros/me.ehf", target_dir
+    system "curl -L -o help_ehf.zip #{url}"
+    # Unzip the downloaded file
+    system "unzip -o help_ehf.zip -d #{buildpath}"
+
+    # Install the license and README files
+    cp "COPYING.txt", target_dir 
+    cp "readme.txt", target_dir 
+    # Install help file
+    cp "macros/me.ehf", target_dir
+    # Clean up the downloaded ZIP file
+    rm_f "help_ehf.zip"    # Create the target directory if it doesn't exist
   end
 
 end
